b1d98bc2cee22f2f1d814a2cbbd0dc276721e398
“TinkerBuild“ <zeyao1994@gmail.com>
2018-03-27 20:27:42 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,9 +14,13 @@ class GroupsController < ApplicationController
    #new add crud
     def create
       @group = Group.new(group_params)
-      @group.save
 
+      #validate
+      if @group.save
         redirect_to groups_path
+      else
+        render :new
+      end
     end
 
   def edit
